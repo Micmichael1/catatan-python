3bae62ea360e07fd5a4e7f30f9e544cd69ef8096
Micmichael1 <lmicmichael21@gmail.com>
2024-04-06 13:13:42 +0700

    Add File OOP.py

---
+++ OOP.py
@@ -0,0 +1,29 @@
+class car():
+    def __init__(self,brand,model) -> None:
+        self.brand = brand
+        self.model = model
+    def promote(self):
+        print(self.brand,self.model)
+    
+    def secret(self):
+        pass
+
+class electric_car(car):
+    def __init__(self, brand="tesla", model="tesla", battery="tesla") -> None:
+        super().__init__(brand, model)
+        self.battery = battery
+
+    def promote(self):
+        print(self.brand,str(self.battery),self.model)
+
+    def secret(self):
+        print("Bisa Berjalan Di Air")
+
+class bekerja():
+    def __init__(self,car) -> None:
+        car.promote()
+        car.secret()
+
+jenis = [car("Mazda","RX8"),electric_car("tesla",model='Truck',battery='8V')]
+for rekursif_jenis in jenis:
+    bekerja(rekursif_jenis)
